e409acf78f9f4e15e92d755073218acc4366e5f4
msalli <alli.cernoch@gmail.com>
2014-07-29 09:38:21 -0700

    database set up

--- models/index.js
+++ models/index.js
@@ -23,6 +23,37 @@ Object.keys(db).forEach(function(modelName) {
   }
 })
 
+//Associations
+db.author.hasMany(db.post);
+db.post.belongsTo(db.author);
+
+//Create function, to see if the associations we have set up are working
+db.post.create({content: "NEW POST NEW POST!"})
+  .success(function (post) {
+    db.author.find(1)
+    .success(function (author) {
+      //clobbering the old posts
+      //maybe there is a getPosts, and then push to an array? 
+      //instances documentation might be helpful? 
+      author.setPosts([post])
+      .success(function (author) {
+        console.log(author)
+      })
+    });
+  });
+
+//test test
+// db.post.create({content: "This is a post, this is a post!"})
+//   .success(function (postObj){
+//     console.log("postObj", postObj);
+//   });
+
+//   db.author.create({name: "Emily Sanderson"})
+//     .success(function (authorObj) {
+//       console.log("this is our author object", authorObj);
+//     });
+
+
 module.exports = lodash.extend({
   sequelize: sequelize,
   Sequelize: Sequelize
